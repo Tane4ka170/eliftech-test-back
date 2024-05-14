3a161cf6e7c95d6939601ab31f6702ad25d29b31
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-14 20:48:05 +0300

    Wrote controllers

---
+++ controllers/events.js
@@ -0,0 +1,42 @@
+import { ctrlWrapper } from "../helpers";
+import { Event } from "../models/event";
+
+const getEvents = async (req, res) => {
+  const { page, limit } = req.query;
+
+  const skip = (page - 1) * limit;
+
+  const events = await Event.find({}, "-updatedAt -createdAt -participants", {
+    skip,
+    limit,
+  });
+
+  res.status(200).json(events);
+};
+
+const addParticipants = async (req, res) => {
+  const { id } = req.params;
+
+  const newUser = await Event.findByIdAndUpdate(
+    id,
+    {
+      $push: { participants: req.body },
+    },
+    { new: true }
+  );
+  res.status(201).json(newUser);
+};
+
+const getEventParticipants = async (req, res) => {
+  const { id } = req.params;
+
+  const participants = await Event.findById(id, "-updatedAt -createdAt");
+
+  res.status(200).json(participants);
+};
+
+export default {
+  getEvents: ctrlWrapper(getEvents),
+  addParticipants: ctrlWrapper(addParticipants),
+  getEventParticipants: ctrlWrapper(getEventParticipants),
+};
