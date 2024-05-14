03caa871d0c3c4df50193d88f186c6250b4e206e
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-05-14 20:47:46 +0300

    started

---
+++ server.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import app from "./app.js";
+
+const { DB_HOST } = process.env;
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(7391, () => {
+      console.log("Server running. Use our API on port: 7391");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
